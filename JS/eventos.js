d8d27811293355410c9a5c953c92c614ac4caf38
JuliRosas17 <rosas.pascual.jv@gmail.com>
2023-01-25 23:52:06 -0300

    comienzo del proyecto

---
+++ JS/eventos.js
@@ -0,0 +1,34 @@
+function enBlack1() {
+Element =document.getElementById("pers");
+Element.src="img/personal.png";
+}
+
+function enBlack2() {
+    Element =document.getElementById("labo");
+    Element.src="img/laboral.png";
+}
+
+function enBlack3() {
+    Element =document.getElementById("habi");
+    Element.src="img/habilidades.png";
+}
+
+function enBlacj4() {
+    Element =document.getElementById("estu");
+    Element.src="img/estudios.png";
+}
+
+function enBlack5() {
+    Element =document.getElementById("be");
+    Element.src="img/twitter.png";
+}
+
+function enBlack6() {
+    Element =document.getElementById("gi");
+    Element.src="img/github.png";
+}
+
+function enBlack7() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin.png";
+}
